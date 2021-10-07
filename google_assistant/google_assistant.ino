df36b03a08cf63653b702c23f1c6c7fbf2d4f16a
Abidullah Mafaza <57884323+zaolyn@users.noreply.github.com>
2021-10-07 14:43:46 +0700

    Add files via upload

---
+++ google_assistant/google_assistant.ino
@@ -0,0 +1,20 @@
+#define BLYNK_PRINT Serial // digunakan untuk monitor blynk di serial
+#include <ESP8266WiFi.h> // file untuk modul wifi
+#include <BlynkSimpleEsp8266.h> // file untuk blynk nya
+
+char auth[] = "YourAuthToken"; // token yang dikirim dari blynk ke email masing masing
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
